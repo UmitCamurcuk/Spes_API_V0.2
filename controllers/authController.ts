e87519ccd6ebfb4a2314857e6f2b16f1dbed6f52
UmitCamurcuk <umtcmrck@icloud.com>
2024-11-14 17:05:56 +0300

    PCHANGES

--- controllers/authController.ts
+++ controllers/authController.ts
@@ -3,6 +3,16 @@ import jwt from 'jsonwebtoken';
 import SystemUser from '../models/systemUserModel'; // Model dosyanızı burada tanımlayın
 import bcrypt from 'bcrypt';
 
+interface LoginResponse { 
+  user: {
+    token: string;
+    first_name : string;
+    middle_name? : string;
+    last_name : string;
+    username: string
+  }
+}
+
 export const loginUser = async (req: Request, res: Response): Promise<Response | void> => {
   const { username, password } = req.body;
 
@@ -22,12 +32,28 @@ export const loginUser = async (req: Request, res: Response): Promise<Response |
     }
 
     // JWT oluştur
-    const token = jwt.sign({ id: user._id, username: user.username }, process.env.JWT_SECRET as string, {
-      expiresIn: '1h', // 1 saat geçerli
+    const token = jwt.sign(
+      { id: user._id, username: user.username },
+      process.env.JWT_SECRET as string,
+      { expiresIn: '1h' }
+    );
+
+    res.cookie('token', token, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === 'production', // Sadece HTTPS üzerinden geçiş yapmasını sağlıyor
+      sameSite: 'strict', // CSRF koruması için
+      maxAge: 3600000, // 1 saat
     });
 
-    res.json({ token });
-    return;
+    return res.json({
+      user: {
+        first_name: user.first_name,
+        middle_name: user.middle_name,
+        last_name: user.last_name,
+        username: user.username
+      }
+    });
+
   } catch (err) {
      res.status(500).json({ message: 'An error occurred' });
      return;
--- server.ts
+++ server.ts
@@ -4,10 +4,16 @@ import express from 'express';
 import connectDB from './config/db';
 import userRouter from './routes/systemUserRoutes';
 import authRouter from './routes/authRoutes';
+import cors from 'cors'
 
 const port = process.env.PORT;
 const app = express();
 app.use(express.json());
+app.use(cors({
+  origin: 'http://localhost:3000',  // React frontend'inizin adresi
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],  // Desteklenen HTTP metotları
+  credentials: true, 
+}));
 
 async function startServer() {
   await connectDB(); // MongoDB'ye bağlanın
